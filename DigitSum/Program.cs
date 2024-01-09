f16b77dba4a13edb3fe1d726af86a8f8ddd9a541
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-01-09 16:42:51 +0100

    sum of digits numbers, recurssion

---
+++ DigitSum/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace DigitSum
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine(SumDigitRec(123456789));
+        }
+        public static int SumDigitRec(int num)
+        {
+            if (num/10<1) return num;
+            return num%10+ SumDigitRec(num/10);
+        }
+    }
+}
