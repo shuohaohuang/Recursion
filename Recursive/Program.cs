154d0edd0d96159ec195888c26832c57411b179e
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-09 12:32:17 +0100

    recursion sum

--- Recursive/Program.cs
+++ Recursive/Program.cs
@@ -6,7 +6,14 @@ namespace Recursion
     {
         public static void Main()
         {
+            Console.WriteLine(SumRec(5));
+        }
 
+        public static int SumRec(int sum)
+        {
+            if (sum == 0) return 0;
+
+            return sum+ SumRec(sum-1);
         }
     }
 }
