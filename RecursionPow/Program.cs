3fe18e9af8f8a430d8ea934392a666ee617e7111
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-09 12:35:44 +0100

    recursion power

---
+++ RecursionPow/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        public static void Main()
+        {
+            Console.WriteLine(PowRec(5,2));
+        }
+
+        public static int PowRec(int basse, int power)
+        {
+            if (power == 0) return 1;
+
+            return basse * PowRec(basse,power - 1);
+        }
+    }
+}
