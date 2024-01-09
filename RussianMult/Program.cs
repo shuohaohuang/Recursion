4b92eb446ff475412ff8370c22938f79bc206c2a
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-09 13:03:22 +0100

    russian mult

--- RussianMult/Program.cs
+++ RussianMult/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        public static void Main()
+        {
+            Console.WriteLine(RussianMult(12,12));
+        }
+
+        public static int RussianMult(int num, int secondNum)
+        {
+            if (num==1) return secondNum;
+            if (num % 2 == 1) return secondNum + RussianMult(num / 2, secondNum * 2);
+            return RussianMult(num/2,secondNum*2);
+        }
+    }
+}
