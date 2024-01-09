c09b3414c0fc107d97bcae2545d38171166bf4da
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-09 13:55:56 +0100

    Recursive binari

---
+++ binari/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        public static void Main()
+        {
+            Console.WriteLine(BinariRec(5));
+        }
+
+        public static int BinariRec(int num)
+        {
+            if (num == 0) return 0;
+            if (num == 1) return 1;
+            return num%2+BinariRec(num/2)*10;
+
+        }
+    }
+}
