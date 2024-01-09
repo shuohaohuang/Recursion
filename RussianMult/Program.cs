3766df3109ace5a13282d3668f969fa6859d31a9
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-09 13:09:50 +0100

    russian mult iterative

--- RussianMult/Program.cs
+++ RussianMult/Program.cs
@@ -7,8 +7,18 @@ namespace Recursion
         public static void Main()
         {
             Console.WriteLine(RussianMult(12,12));
+            Console.WriteLine(RussianMultIterative(12, 12));
         }
-
+        public static int RussianMultIterative(int num, int secondNum)
+        {
+            int result = 0;
+            for (int i = num; i >=1; i/=2) 
+            {
+                if (i % 2 == 1) result += secondNum;
+                secondNum*=2;
+            }
+            return result;
+        }
         public static int RussianMult(int num, int secondNum)
         {
             if (num==1) return secondNum;
