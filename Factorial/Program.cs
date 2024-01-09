21362409b396ced00fcf1418f9b1200b254427c7
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-09 12:43:21 +0100

    Recurssion factorial

---
+++ Factorial/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        public static void Main()
+        {
+            Console.WriteLine(FactRec(6));
+        }
+
+        public static int FactRec(int factrial)
+        {
+            if (factrial == 0|| factrial ==1) return 1;
+
+            return factrial * FactRec(factrial - 1);
+        }
+    }
+}
