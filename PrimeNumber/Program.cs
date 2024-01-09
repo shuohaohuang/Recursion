05036b5c4d990385d7631e75ac9e9e06e29ab9e8
shuohaohuang <shuohao.huang.7e7@itb.cat>
2024-01-09 17:35:01 +0100

    prime number recurssion

---
+++ PrimeNumber/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace PrimeNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Equal(37));
+        }
+        public static int Prime(int num, int prime =2)
+        {
+            if (num == prime) return num;
+            if (num < 2) return 0;
+            if (num % prime == 0)
+                return 0;
+            return Prime(num, prime + 1);
+        }
+
+        public static bool Equal(int num)
+        {
+           return num == Prime(num);
+        }
+    }
+}
