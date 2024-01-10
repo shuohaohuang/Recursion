cf0a5acadd4943abf7dd58ebef725f90d34e4932
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-10 12:11:20 +0100

    a

---
+++ Lyric/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Recursion
+{
+    class Recursion
+    {
+        public static void Main()
+        {
+            const string Lyrics = "{0} pometes té el pomer,\r\nde {0} una, de {0} una," +
+                "\r\n{0} pometes té el pomer," +
+                "\r\nde {0} una en caigué." +
+                "\r\n" +
+                "\r\nSi mireu el vent d’on vé" +
+                "\r\nveureu el pomer com dansa," +
+                "\r\nsi mireu el vent d’on vé" +
+                "\r\nveureu com dansa el pomer.";
+            //apples=pometes,apple=pometes
+            int input=Convert.ToInt32(Console.ReadLine());
+            LyricsRec(Lyrics,input);
+        }
+
+        public static void LyricsRec(string lyric,int num)
+        {
+            Console.WriteLine(lyric,num);
+            if (num == 1) return;
+            LyricsRec(lyric,num-1);
+
+        }
+    }
+}
--- PrimeNumber/Program.cs
+++ PrimeNumber/Program.cs
@@ -4,18 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(Equal(37));
+            Console.WriteLine(IsPrime(37));
         }
         public static int Prime(int num, int prime =2)
         {
+            if (num < 2 || num % prime == 0||num<0) return 0;
             if (num == prime) return num;
-            if (num < 2) return 0;
-            if (num % prime == 0)
-                return 0;
+        
             return Prime(num, prime + 1);
         }
 
-        public static bool Equal(int num)
+        public static bool IsPrime(int num)
         {
            return num == Prime(num);
         }
