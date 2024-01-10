a2e0166871d8e5745008a8356fa7227552f04066
shuohaohuang <shuohao.huang,7e7@itb.cat>
2024-01-10 12:22:35 +0100

    a

--- DigitSum/Program.cs
+++ DigitSum/Program.cs
@@ -4,12 +4,12 @@
     {
         static void Main()
         {
-            Console.WriteLine(SumDigitRec(123456789));
+            Console.WriteLine(SumDigitRec(10));
         }
         public static int SumDigitRec(int num)
         {
             if (num/10<1) return num;
-            return num%10+ SumDigitRec(num/10);
+            return num%10 + SumDigitRec(num/10);
         }
     }
 }
